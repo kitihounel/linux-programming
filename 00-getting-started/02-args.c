59fb9322cb19e181cd066e271ad5d693192edd58
kitihounel <kitihounel@gmail.com>
2025-06-26 14:11:11 +0100

    feat (intro): output env variables

--- 00-getting-started/02-args.c
+++ 00-getting-started/02-args.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *envp[])
 {
+    char **tmp = envp;
+
     printf("number of arguments: %d\n", argc);
     for (int i = 0; i < argc; ++i) {
         printf("%s\n", argv[i]);
+    }
+
+    printf("environment variables:\n");
+    while (*tmp != NULL) {
+        printf("%s\n", *tmp);
+        ++tmp;
     }
 
     return 0;
